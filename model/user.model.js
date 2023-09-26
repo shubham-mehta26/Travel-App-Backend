df9e75105c58d744c8fbf4a77b25532526887942
Shubham Mehta <shubham.sm911@gmail.com>
2023-09-26 18:16:06 +0530

    added timestamp for user creation

--- model/user.model.js
+++ model/user.model.js
@@ -6,6 +6,9 @@ const userSchema = new mongoose.Schema({
     number: {type: Number, required: true , unique: true},
     email : {type: String, required: true , unique: true},
     password : {type: String, required: true}
+},
+{
+    timestamps: true,
 });
 
 const User = mongoose.model("User",userSchema);
