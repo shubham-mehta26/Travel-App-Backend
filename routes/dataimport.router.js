9355e37a361dab48914a3815bc90bd97d59e6f3f
Shubham Mehta <shubham.sm911@gmail.com>
2023-10-11 11:32:57 +0530

    data import correct

--- routes/dataimport.router.js
+++ routes/dataimport.router.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.route("/")
     .post(async (req,res)=>{
         try{
-            await Hotel.remove();
+            // await Hotel.remove();
             const hotelsInDB = await Hotel.insertMany(hotel.data);
             res.json(hotelsInDB);
         }
